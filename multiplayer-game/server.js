c5f61f8369b031119de5caa89b51f21005818476
jhughescu <jhughes@cranfield.ac.uk>
2023-07-27 13:51:45 +0100

    overwriting bad merge

--- multiplayer-game/server.js
+++ multiplayer-game/server.js
@@ -47,7 +47,6 @@ io.on('connection', (socket) => {
         }
     });
     const removal = () => {
-        console.log('removal');
         // Remove the player ID from the 'players' map upon disconnection
         for (const [servantId, servantSocket] of servants.entries()) {
             if (servantSocket === socket) {
@@ -55,27 +54,19 @@ io.on('connection', (socket) => {
                 break;
             }
         }
-<<<<<<< HEAD
         const servantIds = Array.from(servants.keys());
         console.log('emitting getServants');
         io.emit('getServants');
         io.emit('updateServants', servantIds);
-=======
-        console.log(`call getServants`);
-        socket.emit('getServants');
-        console.log(`called getServants`);
->>>>>>> origin/main
     }
     socket.on('remove', () => {
         removal();
     });
     socket.on('disconnect', () => {
-        console.log('disconnect');
         removal();
     });
     socket.on('getServants', () => {
         // Send the list of player IDs to the requesting client
-        console.log('get em');
         const servantIds = Array.from(servants.keys());
         console.log('emitting onGetServants');
         io.emit('onGetServants', servantIds);
